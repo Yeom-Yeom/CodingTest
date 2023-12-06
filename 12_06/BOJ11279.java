6a604967751dbef6496db0262e97cd2eb1f3d379
Yeom <a1004@Yeoms-MacBook-Air.local>
2023-12-06 21:22:44 +0900

    23.12.06

---
+++ 12_06/BOJ11279.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class BOJ11279 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0; i<n; i++){
+            int x = sc.nextInt();
+            if(x==0 && pq.isEmpty()){
+                System.out.println(0);
+            }else if(x==0){
+                System.out.println(pq.peek());
+                pq.poll();
+            }
+            else{
+                pq.add(x);
+            }
+        }
+    }
+}
