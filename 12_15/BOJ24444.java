a640040149e35d44fa308dc128a557c578981bba
Yeom <a1004@Yeoms-MacBook-Air.local>
2023-12-15 22:12:26 +0900

    23.12.15

---
+++ 12_15/BOJ24444.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+public class BOJ24444 {
+    static int[] ans;
+    static ArrayList<Integer>[] edges;
+    static int idx=0;
+    static int[] v;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        edges = new ArrayList[n+1];
+        for(int i=1; i<=n; i++){
+            edges[i] = new ArrayList<>();
+        }
+        ans = new int[n+1];
+        v = new int[n+1];
+        while(m-->0){
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            edges[u].add(v);
+            edges[v].add(u);
+        }
+        for(int i=1; i<=n; i++){
+            Collections.sort(edges[i]);
+        }
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(r);
+        int visitCnt = 1;
+        v[r] = visitCnt;
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            for(int next : edges[cur]){
+                if(v[next]!=0) continue;
+                v[next] = ++visitCnt;
+                q.add(next);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=n; i++){
+            sb.append(v[i]).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
